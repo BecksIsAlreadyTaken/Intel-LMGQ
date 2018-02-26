a39918756712b4510184ef4b0436db5eeeea4609
beck <503646976@qq.com>
2018-02-26 18:13:33 +0800

    Update image processing feature.

--- js/background.js
+++ js/background.js
@@ -1,73 +1,86 @@
 var toggle;
-
 if (!localStorage["blocked"]) {
     localStorage["blocked"] = false;
 }
 
-// if (!localStorage["activate_for_currentUrl"]) {
-//     localStorage["activate_for_currentUrl"] = true;
-// }
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
-        if (request.toBg == "true") {
-            if (JSON.parse(localStorage["blocked"]) === false) {
-                var reg = new RegExp("chrome://");
-                var flag = reg.test(sender.tab.url);
-                if (flag) {
-
-                } else {
-                    chrome.storage.sync.get(null, function(items) { //check whitelist
-                        let f = false;
-                        for (i in items.whitelist) {
-                            if (items.whitelist[i].url == sender.tab.url) {
-                                f = true;
-                                break;
-                            }
-                        }
-                        chrome.storage.local.remove(["inWl", "currentUrl"], function() {}); //remove local storage if any
-                        chrome.storage.local.set({ 'inWl': f, 'currentUrl': sender.tab.url }, function() {}); //using local storage to store boolean inWl and current url of the tab
-                        chrome.storage.local.get(null, function(items) { console.log(items); }); //get temp variable stored in local storage
-                        chrome.tabs.sendMessage(sender.tab.id, { state: "on" }, function(response) { console.log(response); });
-                    });
-                }
-            } else {
-                chrome.browserAction.setIcon({ path: "off.png", tabId: sender.tab.id });
-                chrome.tabs.sendMessage(sender.tab.id, { state: "off" }, function(response) { console.log(response); }); //send a message to content script
-            }
-        }
-    });
-
 chrome.browserAction.onClicked.addListener(function(tab) {
-    var reg = new RegExp("chrome://");
-    var flag = reg.test(tab.url);
-    if (flag) {
-
+    localStorage["blocked"] = !JSON.parse(localStorage["blocked"]);
+    toggle = !JSON.parse(localStorage["blocked"]);
+    if (toggle) {
+        chrome.browsingData.remove({
+            since: 0
+        }, {
+            cache: true,
+            appcache: true,
+        }, function() {
+            console.log("remove");
+        });
+        chrome.browserAction.setIcon({
+            path: "images/on.png"
+        });
     } else {
-        localStorage["blocked"] = !JSON.parse(localStorage["blocked"]);
-        toggle = !JSON.parse(localStorage["blocked"]);
-        if (toggle) {
-            chrome.browserAction.setIcon({ path: "on.png", tabId: tab.id });
+        chrome.browserAction.setIcon({
+            path: "images/off.png"
+        });
+    }
+});
+
+chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    if (JSON.parse(localStorage["blocked"])) {
+        localStorage["current_in_WL"] = true;
+    } else {
+        chrome.tabs.query({
+            'active': true
+        }, function(tabs) {
             chrome.storage.sync.get(null, function(items) { //check whitelist
                 let f = false;
                 for (i in items.whitelist) {
-                    if (items.whitelist[i].url == tab.url) {
+                    if (items.whitelist[i].url == tabs[0].url) {
                         f = true;
                         break;
                     }
                 }
-                chrome.storage.local.remove(["inWl", "currentUrl"], function() {}); //remove local storage if any
-                chrome.storage.local.set({ 'inWl': f, 'currentUrl': tab.url }, function() {}); //using local storage to store boolean inWl and current url of the tab
-                chrome.storage.local.get(null, function(items) { console.log(items); }); //get temp variable stored in local storage
-                chrome.tabs.sendMessage(tab.id, { state: "on" }, function(response) { console.log(response); }); //send a message to content script
+                if (f) localStorage["current_in_WL"] = true;
+                else localStorage["current_in_WL"] = false;
             });
-
-        } else {
-            chrome.browserAction.setIcon({ path: "off.png", tabId: tab.id });
-            chrome.tabs.reload(tab.id);
-        }
+        });
+        // check if base64 then add tag
+        localStorage[details.url] = true; // interacting with server
     }
-});
+}, {
+    urls: ["<all_urls>"],
+    types: ["image"]
+}, ["blocking"]);
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function(details) {
+        var whitelistChecked;
+        var urlChecked;
+        if (typeof localStorage["current_in_WL"] !== "undefined" &&
+            localStorage["current_in_WL"] !== "undefined") {
+            whitelistChecked = JSON.parse(localStorage["current_in_WL"]);
+        }
+        if (typeof localStorage[details.url] !== "undefined" &&
+            localStorage[details.url] !== "undefined") {
+            urlChecked = JSON.parse(localStorage[details.url]);
+        }
+        // chrome url check
+        // if base64 
+        localStorage.removeItem(details.url);
+        if (whitelistChecked) {
+
+        } else if (urlChecked) return {
+            redirectUrl: chrome.runtime.getURL('images/blocked.svg')
+        };
+    }, {
+        urls: ["<all_urls>"],
+        types: ["image"]
+    }, ["blocking"]
+);
+
+chrome.webRequest.onResponseStarted.addListener(function(details) {
+    // gif filtering
+}, {
+    urls: ["<all_urls>"],
+    types: ["image"]
+}, ["responseHeaders"]);
--- js/content.js
+++ js/content.js
@@ -10,41 +10,53 @@ function getImgUrl() {
     console.log(document.images.length); // img num
 }
 
-function getIframeImgUrl() {}
+function getIframeImgUrl() {
 
-function getBgImgUrl() {}
+}
 
-function getAjaxImgUrl() {}
+function getBgImgUrl() {
+
+}
+
+function getAjaxImgUrl() {
+
+}
+
+function getDataSrc(){
+    
+}
 
 function blockNsfwImgs() {
-    console.log("nsfw");
+    //console.log("nsfw");
 }
 
 function connectToServer() {}
 
-function returnToNormal() {
+function Normal() {
     console.log("off");
 }
-
-chrome.runtime.sendMessage({ toBg: "true" }, function(response) {});
-
+// chrome.runtime.sendMessage({ toBg: "true" }, function(response) {});
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log(sender.tab ?
             "from a content script:" + sender.tab.url :
             "from the extension");
-        if (request.state === "on") {
-            getImgUrl();
-            chrome.storage.local.get(null, function(items) { //get current url and start processing the imgs
-                console.log(items);
-                if (items.inWl) {} else {
-                    blockNsfwImgs();
-                }
-            });
-            chrome.storage.local.remove(["inWl", "currentUrl"], function() {});
-            sendResponse({ resp: 'turn on' });
-        } else if (request.state === "off") {
-            returnToNormal();
-            sendResponse({ resp: 'turn off' });
-        }
+        // if (request.state === "on") {
+        //     console.log(request.url);
+        //     //getImgUrl();
+        //     // chrome.storage.local.get(null, function(items) { //get current url and start processing the imgs
+        //     //     console.log(items);
+        //     //     if (items.inWl) {
+        //     //         Normal();
+        //     //     } else {
+        //     //         blockNsfwImgs();
+        //     //     }
+        //     // });
+        //     // chrome.storage.local.remove(["inWl", "currentUrl"], function() {});
+        //     blockNsfwImgs();
+        //     sendResponse({ resp: 'turn on' });
+        // } else if (request.state === "off") {
+        //     Normal();
+        //     sendResponse({ resp: 'turn off' });
+        // }
     });
